52372b369a35af68d40873d62703f13fff0e6c6d
Roberto Carlos Jacinto Chirinos <robertojch.net@gmail.com>
2019-09-17 00:37:15 -0500

    Adding datos Productivos

--- src/app/custom-table/custom-table.component.ts
+++ src/app/custom-table/custom-table.component.ts
@@ -41,51 +41,77 @@ const COUNTRIES: Country[] = [
 })
 export class CustomTableComponent implements OnInit {
   countries;
-  columnsDefs: any[];
-  rowData: any[];
+  private columnsDefs1: any[];
+  private rowData1: any[];
+  gridOptions: any;
   constructor() {}
 
   ngOnInit() {
     this.countries = [];
-    this.columnsDefs = [
-      { field: 'ali_dia', headerName: 'Alimentaci&oacute;n <br>por d&iacute;a', editable: true, width: '104', sortable: true },
+    this.columnsDefs1 = [
+      { field: 'ali_dia', headerName: 'Alimentaci&oacute;n <br>por d&iacute;a', cellEditor: 'input', width: '104', sortable: true },
       { field: 'ali_type', headerName: 'Tipo de alimento', width: '174', editable: false},
       { field: 'ali_hec', headerName: 'Alimentaci&oacute;n <br>por hectarea', width: '175', editable: false},
-      { field: 'ali_peso', headerName: 'Peso',  width: '104', editable: true},
+      { field: 'ali_peso', headerName: 'Peso',  width: '104', cellEditor: 'input' , editable: true},
       { field: 'ali_inc', headerName: 'Incremento <br>acumulado', width: '100', editable: false},
-      { field: 'ali_sup', headerName: 'Supervivencia', width: '100', editable: true},
-      { field: 'ali_blo', headerName: 'Blomasa por hect&aacute;rea',  width: '100', editable: false}
+      { field: 'ali_sup', headerName: 'Supervivencia', width: '100', cellEditor: 'input', editable: true},
+      { field: 'ali_blo', headerName: 'Blomasa por hect&aacute;rea',   width: '100', editable: false}
     ];
 
-    this.rowData = [
+    this.rowData1 = [
       {
-        ali_dia: '12.34',
-        ali_type: '-',
-        ali_hec: '-',
-        ali_peso: '12.34',
-        ali_inc: '12.34',
-        ali_sup: '12.34',
-        ali_blo: '12.34',
+        ali_dia: { value: '525', um: 'kg/dia'},
+        ali_type: { value: '-'},
+        ali_hec: { value: '120', um: 'kg/Ha'},
+        ali_peso: { value: '6.24', um: 'g'},
+        ali_inc: { value: '2.34', um: 'g/sem'},
+        ali_sup: { value: '64', um: '%'},
+        ali_blo: { value: '9400', um: 'lb/ha'},
       },
       {
-        ali_dia: '12.34',
-        ali_type: '-',
-        ali_hec: '-',
-        ali_peso: '12.34',
-        ali_inc: '12.34',
-        ali_sup: '12.34',
-        ali_blo: '12.34',
+        ali_dia: { value: '525', um: 'kg/dia'},
+        ali_type: { value: '-'},
+        ali_hec: { value: '120', um: 'kg/Ha'},
+        ali_peso: { value: '6.24', um: 'g'},
+        ali_inc: { value: '2.34', um: 'g/sem'},
+        ali_sup: { value: '64', um: '%'},
+        ali_blo: { value: '9400', um: 'lb/ha'},
       },
       {
-        ali_dia: '12.34',
-        ali_type: '-',
-        ali_hec: '-',
-        ali_peso: '12.34',
-        ali_inc: '12.34',
-        ali_sup: '12.34',
-        ali_blo: '12.34',
+        ali_dia: { value: '525', um: 'kg/dia'},
+        ali_type: { value: '-'},
+        ali_hec: { value: '120', um: 'kg/Ha'},
+        ali_peso: { value: '6.24', um: 'g'},
+        ali_inc: { value: '2.34', um: 'g/sem'},
+        ali_sup: { value: '64', um: '%'},
+        ali_blo: { value: '9400', um: 'lb/ha'},
+      },
+      {
+        ali_dia: { value: '525', um: 'kg/dia'},
+        ali_type: { value: '-'},
+        ali_hec: { value: '120', um: 'kg/Ha'},
+        ali_peso: { value: '6.24', um: 'g'},
+        ali_inc: { value: '2.34', um: 'g/sem'},
+        ali_sup: { value: '64', um: '%'},
+        ali_blo: { value: '9400', um: 'lb/ha'},
+      },
+      {
+        ali_dia: { value: '525', um: 'kg/dia'},
+        ali_type: { value: '-'},
+        ali_hec: { value: '120', um: 'kg/Ha'},
+        ali_peso: { value: '6.24', um: 'g'},
+        ali_inc: { value: '2.34', um: 'g/sem'},
+        ali_sup: { value: '64', um: '%'},
+        ali_blo: { value: '9400', um: 'lb/ha'},
       }
     ];
-    console.log(this.rowData);
+
+    this.gridOptions = {
+      columnsDefs: this.columnsDefs1,
+      rowData: this.rowData1,
+      width: 831
+    };
+
+    console.log(this.rowData1);
   }
 }
